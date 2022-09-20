d55584230801610d1c559677fc8788c6fd641a4a
prashant damale <damale.prashant.21@gmail.com>
2022-09-20 16:26:35 +0530

    adding abstraction

---
+++ abstraction/Abstract2.java
@@ -0,0 +1,17 @@
+package com.abstraction;
+
+public class Abstract2 extends Abstract_Class {//to inherit abstract methods class must be abstract
+	//or add unimplemented method
+
+	@Override
+	void m2() {
+		System.out.println("m2 method");
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		m3();
+	}
+}
---
+++ abstraction/Abstract3.java
@@ -0,0 +1,12 @@
+package com.abstraction;
+
+public class Abstract3 {
+	public static void main(String[] args) {
+		Abstract2 obj=new Abstract2();
+		obj.m2();
+		obj.m1();
+		//can't create object of abstract class
+	}
+
+}
+ 
